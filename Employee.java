f2cba59713842648b83b0b1f853e3634a3554fa8
Madhukgit <madhavik7120@gmail.com>
2021-09-06 23:22:31 -0400

    This file is for WH-8856 EMP Module

--- Employee.java
+++ Employee.java
@@ -1,1 +1,23 @@
+public class Employee{
+
+
+public void empdetails{
+	
+	int empNo = 101;
+	String empName ="Ganesha";
+	double empsal 99000.00;
+	
+	System.out.println("Employee No: " + empNo + "Employee Name : " + empName + " Employee Salary : " +  empsal  )
+
+}
+
+	public static void main(String[] args){
+
+		System.out.println("Welcome to Employee Management!");
+		System.out.println("The details of the employee are as follows: ");
+		empdetails
+
+	}
+
+
 
